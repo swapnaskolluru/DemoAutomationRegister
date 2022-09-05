26b71210f4bc6ae246abda79186bd69f396185da
swapnaskolluru <112838015+swapnaskolluru@users.noreply.github.com>
2022-09-05 04:48:15 +0100

    Update TestRunner.java

--- RegistrationForm/src/test/java/stepDefinition/TestRunner.java
+++ RegistrationForm/src/test/java/stepDefinition/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "RegistrationForm/src/test/java/feature",glue = "stepDefinition")
+@CucumberOptions(features = "src/test/java/feature",glue = "stepDefinition")
 public class TestRunner 
 {
 	
